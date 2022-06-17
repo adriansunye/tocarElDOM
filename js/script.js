a0e778ca58c91aa99c749bc91aae9c28c455bf05
AlessHub <aless312@gmail.com>
2022-06-17 09:19:00 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
- const body = document.querySelector('body'),
- overlay = body.querySelector(".overlay"),
- icon = body.querySelector(".icon"),
- iconfade = body.querySelector(".icon-fade"),
- instrumentos = body.querySelector(".instrumentosimg");
+const body = document.querySelector('body'),
+overlay = body.querySelector(".overlay"),
+icon = body.querySelector(".icon"),
+iconfade = body.querySelector(".icon-fade"),
+instrumentos = body.querySelector(".instrumentosimg");
 
- overlay.addEventListener("mouseover", () => {
-    icon.classList.toggle("icon-fade");
- });
+overlay.addEventListener("mouseover", () => {
+   icon.classList.toggle("icon-fade");
+});
 
- overlay.addEventListener("mouseout", () => {
-  icon.classList.toggle("icon-fade");
+overlay.addEventListener("mouseout", () => {
+ icon.classList.toggle("icon-fade");
 });
 
 
 
-  // document.querySelectorAll(".icon").forEach(icon => icon.classList.toggle("icon-fade")))
+ // document.querySelectorAll(".icon").forEach(icon => icon.classList.toggle("icon-fade")))
